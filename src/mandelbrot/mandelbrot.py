11fa5fe586f261b96e50d31c252f2610b369f03e
viviseve <viviseve9@gmail.com>
2022-10-20 10:54:37 +0200

    Mise à jour Mandelbrot

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -16,14 +16,24 @@ def suite_julia(z:complex, c:complex) -> bool:
     return suite(z, c)
 
 def is_in_Mandelbrot(c:complex, max_iter:int=50):
+    """Calcule une suite de Mandelbrot et vérifie la non-divergence avec la condition $|z_n| <= 2 $
+    >>> is_in_Mandelbrot(c=0.251)
+    True
+    >>> is_in_Mandelbrot(c=0.251,max_iter=100)
+    False
+    """
     Z = suite_mandelbrot(c)
     i = 0 
     while abs(next(Z)) <= 2 and i < max_iter:
         i+= 1
     return i == max_iter
 
-def is_in_Julia(z:complex, c:complex, eps:float = 1e-5,max_iter:int=50):
-    Z = suite_julia(z,c)
+def is_in_Julia(z:complex, c:complex, max_iter:int=50, eps:float=1e-3):
+    """ Calcule une suite de Julia et vérifie la non-divergence, ainsi que la convergence des deux derniers termes
+    >>> is_in_Julia(z=0.25+0.25j,c=0.25)
+    True
+    """
+    Z = suite_julia(z, c)
     i = 0 
     z_old = z 
     z_cur = next(Z)
@@ -32,3 +42,7 @@ def is_in_Julia(z:complex, c:complex, eps:float = 1e-5,max_iter:int=50):
         z_cur = next(Z)
         i+= 1
     return i == max_iter and abs(z_cur - z_old) <= eps 
+
+if __name__ == "__main__" :
+    import doctest
+    print(doctest.testmod(verbose=1))
