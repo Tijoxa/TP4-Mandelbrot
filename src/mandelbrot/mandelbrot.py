f2a7a900cf8a0c387d8b24af1a831b7b892a1e93
viviseve <viviseve9@gmail.com>
2022-10-20 10:24:19 +0200

    Test

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -6,11 +6,25 @@ def suite(z, c) -> complex:
         yield z
         z = z ** 2 + c
 
-def suite_mandelbrot(z=0, c=candidat) -> bool:
+def suite_mandelbrot(c:complex) -> bool:
     """Renvoie la suite de Mandelbrot"""
-    return suite(z, c)
+    return suite(0, c)
 
-def suite_julia(z=candidat, c=parametre) -> bool:
+def suite_julia(z:complex, c:complex) -> bool:
     """Renvoie la suite de julia pour candidat et parametre"""
     ...
     return suite(z, c)
+
+def is_in_Mandelbrot(c:complex, max_iter:int=50):
+    Z = suite_mandelbrot(c)
+    i = 0 
+    while abs(next(Z)) <= 2 and i < max_iter:
+        i+= 1
+    return i == max_iter
+
+def is_in_Julia(z:complex, c:complex, max_iter:int=50):
+    Z = suite_julia(z,c)
+    i = 0 
+    while abs(next(Z)) <= 2 and i < max_iter:
+        i+= 1
+    return i == max_iter
