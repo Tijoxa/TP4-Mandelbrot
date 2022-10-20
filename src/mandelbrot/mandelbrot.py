0c076f1c7cf61c2c659cd74d20512520dc036c31
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2022-10-21 00:03:28 +0200

    fix plot

--- src/mandelbrot/mandelbrot.py
+++ src/mandelbrot/mandelbrot.py
@@ -11,26 +11,25 @@ def suite_mandelbrot(c:complex) -> bool:
     return suite(0, c)
 
 def suite_julia(z:complex, c:complex) -> bool:
-    """Renvoie la suite de julia pour candidat et parametr, sans vérification"""
-    ...
+    """Renvoie la suite de julia pour candidat et paramètre, sans vérification"""
     return suite(z, c)
 
 def is_in_Mandelbrot(c:complex, max_iter:int=50):
-    """Calcule une suite de Mandelbrot et vérifie la non-divergence avec la condition $|z_n| <= 2 $
+    """Calcule une suite de Mandelbrot et vérifie la non-divergence avec la condition $|z_n| <= 2$
     >>> is_in_Mandelbrot(c=0.251)
     True
-    >>> is_in_Mandelbrot(c=0.251,max_iter=100)
+    >>> is_in_Mandelbrot(c=0.251, max_iter=100)
     False
     """
     Z = suite_mandelbrot(c)
     i = 0 
     while abs(next(Z)) <= 2 and i < max_iter:
-        i+= 1
+        i += 1
     return i == max_iter
 
 def is_in_Julia(z:complex, c:complex, max_iter:int=50, eps:float=1e-3):
     """ Calcule une suite de Julia et vérifie la non-divergence, ainsi que la convergence des deux derniers termes
-    >>> is_in_Julia(z=0.25+0.25j,c=0.25)
+    >>> is_in_Julia(z=0.25+0.25j, c=0.25)
     True
     """
     Z = suite_julia(z, c)
@@ -43,6 +42,6 @@ def is_in_Julia(z:complex, c:complex, max_iter:int=50, eps:float=1e-3):
         i += 1
     return i == max_iter and abs(z_cur - z_old) < eps 
 
-if __name__ == "__main__" :
+if __name__=="__main__":
     import doctest
     print(doctest.testmod(verbose=1))
--- src/mandelbrot/plot.py
+++ src/mandelbrot/plot.py
@@ -1,26 +1,26 @@
-import mandelbrot as mdl # TO DO: fix ça
+import mandelbrot as mdl # TODO: fix l'import si exécution dans un autre cd
 from PIL import Image
 import numpy as np 
 from tqdm import tqdm
 
-def plot_mandelbrot(zmin:complex=1-1j,
+def plot_mandelbrot(zmin:complex=-1-1j,
                     zmax:complex=1+1j,
-                    pixel_size:float=5e-7,
+                    pixel_size:float=5e-4,
                     max_iter:int=50,
                     fig_name:str="fig_mandel"):
-    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax] et enregistre le résultat au format .png
+    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin, zmax] et enregistre le résultat au format .png
     
-    Parametres
+    Paramètres
     ----------
-    zmin : complex, optional
+    zmin: complex, optional
         La borne inférieure des données à évaluer (default vaut -1-1j)
-    zmax : complex, optional 
+    zmax: complex, optional 
         La borne supérieure des données à évaluer (default vaut 1+1j)
-    pixel_size : float, optional
-        La taille d'un pixel dans la figure (default vaut 5e-7)
-    max_iter : int, optional 
+    pixel_size: float, optional
+        La taille d'un pixel dans la figure (default vaut 5e-4)
+    max_iter: int, optional 
         Le nombre d'itérations maximum pour calculer chaque suite de Mandelbrot (default vaut 50)
-    fig_name : str, optional
+    fig_name: str, optional
         Le nom de la figure enregistrée est "fig_name".png (default vaut "fig_mandel")
 
     Retours
@@ -31,44 +31,54 @@ def plot_mandelbrot(zmin:complex=1-1j,
     rmin, imin = zmin.real, zmin.imag
     rmax, imax = zmax.real, zmax.imag
     rdiff, idiff = rmax-rmin, imax-imin
-    X = np.arange(rmin, rmax, pixel_size)
-    Y = np.arange(imin, imax, pixel_size)
+
+    if rdiff > idiff:
+        w, h = int(1/pixel_size), int(idiff/pixel_size/rdiff)
+    else:
+        w, h = int(rdiff/pixel_size/idiff), int(1/pixel_size)
+
+    if rmin != rmax:
+        X = np.linspace(rmin, rmax, w)
+    else:
+        X = np.array([rmin])
+    if imin != imax:
+        Y = np.linspace(imin, imax, h)
+    else:
+        Y = np.array([imin])
+
     ligne, colonne = len(X), len(Y)
     img = Image.new('L', (ligne, colonne), 255)
 
     stream = tqdm(X)
     stream.set_description("Génération de l'image")
-    for x in stream:
-        for y in Y:
+    for enu_x, x in enumerate(stream):
+        for enu_y, y in enumerate(Y):
             c = complex(x, y)
             if mdl.is_in_Mandelbrot(c, max_iter):
-                tup = (int(ligne*(x-rmin)/rdiff), int(colonne*(y-imin)/idiff))
-                img.putpixel(tup, 0)
-    img.resize((2000, 2000))
-    img.show()
-    img.save(f"{fig_name}.png")
+                img.putpixel((enu_x, enu_y), 0)
+    img.save(f"src/mandelbrot/results/{fig_name}.png")
 
 def plot_julia(c:complex,
-               zmin:complex=1-1j,
+               zmin:complex=-1-1j,
                zmax:complex=1+1j,
-               pixel_size:float=5e-7,
+               pixel_size:float=5e-4,
                max_iter:int=50,
                fig_name:str="fig_mandel"):
-    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax] et enregistre le résultat au format .png
+    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin, zmax] et enregistre le résultat au format .png
     
-    Parametres
+    Paramètres
     ----------
-    c : complex
+    c: complex
         Le parametre de calcul des suites de Julia 
-    zmin : complex, optional
+    zmin: complex, optional
         La borne inférieure des données à évaluer (default vaut -1-1j)
-    zmax : complex, optional 
+    zmax: complex, optional 
         La borne supérieure des données à évaluer (default vaut 1+1j)
-    pixel_size : float, optional
-        La taille d'un pixel dans la figure (default vaut 5e-7)
-    max_iter : int, optional 
+    pixel_size: float, optional
+        La taille d'un pixel dans la figure (default vaut 5e-4)
+    max_iter: int, optional 
         Le nombre d'itérations maximum pour calculer chaque suite de Mandelbrot (default vaut 50)
-    fig_name : str, optional
+    fig_name: str, optional
         Le nom de la figure enregistrée est "fig_name".png (default vaut "fig_mandel")
 
     Retours
@@ -79,23 +89,32 @@ def plot_julia(c:complex,
     rmin, imin = zmin.real, zmin.imag
     rmax, imax = zmax.real, zmax.imag
     rdiff, idiff = rmax-rmin, imax-imin
-    X = np.arange(rmin, rmax, pixel_size)
-    Y = np.arange(imin, imax, pixel_size)
+    
+    if rdiff > idiff:
+        w, h = int(1/pixel_size), int(idiff/pixel_size/rdiff)
+    else:
+        w, h = int(rdiff/pixel_size/idiff), int(1/pixel_size)
+
+    if rmin != rmax:
+        X = np.linspace(rmin, rmax, w)
+    else:
+        X = np.array([rmin])
+    if imin != imax:
+        Y = np.linspace(imin, imax, h)
+    else:
+        Y = np.array([imin])
+
     ligne, colonne = len(X), len(Y)
     img = Image.new('L', (ligne, colonne), 255)
 
     stream = tqdm(X)
     stream.set_description("Génération de l'image")
-    for x in stream:
-        for y in Y:
+    for enu_x, x in enumerate(stream):
+        for enu_y, y in enumerate(Y):
             z = complex(x, y)
-            booleen = mdl.is_in_Julia(z, c, max_iter)
-            if booleen:
-                tup = (int(ligne*(x-rmin)/rdiff), int(colonne*(y-imin)/idiff))
-                img.putpixel(tup, 0)
-    img.resize((2000, 2000))
-    img.show()
-    img.save(f"{fig_name}.png")
+            if mdl.is_in_Julia(z, c, max_iter):
+                img.putpixel((enu_x, enu_y), 0)
+    img.save(f"src/mandelbrot/results/{fig_name}.png")
 
 if __name__=='__main__':
     plot_mandelbrot()
