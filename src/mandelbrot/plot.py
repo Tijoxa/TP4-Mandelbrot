49f152216fd4a1c357e7aace0dd923c68be8f4cb
viviseve <viviseve9@gmail.com>
2022-10-20 11:13:13 +0200

    Ajout Doc type Numpy

--- src/mandelbrot/plot.py
+++ src/mandelbrot/plot.py
@@ -1,11 +1,49 @@
 import mandelbrot as mdl 
 
-def plot_mandelbrot(zmin:complex,zmax:complex,pixel_size:float,max_iter:int,fig_name:str):
-    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax]
+def plot_mandelbrot(zmin:complex=1-1j,zmax:complex=1+1j,pixel_size:float=5e-7,max_iter:int=50,fig_name:str="fig_mandel"):
+    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax] et enregistre le résultat au format .png
+    
+    Parametres
+    ----------
+    zmin : complex, optional
+        La borne inférieure des données à évaluer (default vaut -1-1j)
+    zmax : complex, optional 
+        La borne supérieure des données à évaluer (default vaut 1+1j)
+    pixel_size : float, optional
+        La taille d'un pixel dans la figure (default vaut 5e-7)
+    max_iter : int, optional 
+        Le nombre d'itérations maximum pour calculer chaque suite de Mandelbrot (default vaut 50)
+    fig_name : str, optional
+        Le nom de la figure enregistrée est "fig_name".png (default vaut "fig_mandel")
+
+    Retours
+    -------
+    None 
+
     """
     pass
 
-def plot_mandelbrot(c:complex,zmin:complex,zmax:complex,pixel_size:float,max_iter:int,fig_name:str):
-    """ Affiche l'ensemble de Julia pour une plage [zmin,zmax]
+def plot_julia(c:complex,zmin:complex=1-1j,zmax:complex=1+1j,pixel_size:float=5e-7,max_iter:int=50,fig_name:str="fig_mandel"):
+    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax] et enregistre le résultat au format .png
+    
+    Parametres
+    ----------
+    c : complex
+        Le parametre de calcul des suites de Julia 
+    zmin : complex, optional
+        La borne inférieure des données à évaluer (default vaut -1-1j)
+    zmax : complex, optional 
+        La borne supérieure des données à évaluer (default vaut 1+1j)
+    pixel_size : float, optional
+        La taille d'un pixel dans la figure (default vaut 5e-7)
+    max_iter : int, optional 
+        Le nombre d'itérations maximum pour calculer chaque suite de Mandelbrot (default vaut 50)
+    fig_name : str, optional
+        Le nom de la figure enregistrée est "fig_name".png (default vaut "fig_mandel")
+
+    Retours
+    -------
+    None 
+
     """
     pass
