3c1b4554b4ba810e0e8a4e71467992a7144e95af
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2022-10-20 09:20:02 +0200

    setup 2

---
+++ src/__main__.py
@@ -0,0 +1,20 @@
+"""Main call to helloworld. Mostly a parser."""
+import argparse
+from src.helper import hello_world, hello
+
+def main():
+    """Fonction principale"""
+    parser = argparse.ArgumentParser(description='Process a name')
+    parser.add_argument('--name', '-n', metavar='name', type=str,
+                        help='name of the user')
+
+    args = parser.parse_args()
+
+    name=args.name
+    if name is None:
+        hello_world()
+    else:
+        hello(name)
+
+if __name__=="__main__":
+    main()
