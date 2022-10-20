9d43d540d7027e572a78b52c02eb28ba5ef6e50d
viviseve <viviseve9@gmail.com>
2022-10-20 11:04:03 +0200

    Ajout plot.py

---
+++ src/mandelbrot/plot.py
@@ -0,0 +1,11 @@
+import mandelbrot as mdl 
+
+def plot_mandelbrot(zmin:complex,zmax:complex,pixel_size:float,max_iter:int,fig_name:str):
+    """ Affiche l'ensemble de Mandelbrot pour une plage [zmin,zmax]
+    """
+    pass
+
+def plot_mandelbrot(c:complex,zmin:complex,zmax:complex,pixel_size:float,max_iter:int,fig_name:str):
+    """ Affiche l'ensemble de Julia pour une plage [zmin,zmax]
+    """
+    pass
